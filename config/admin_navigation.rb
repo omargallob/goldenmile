0184abba0308d94a2039c46ea6e556d45506d2bd
omar gallo <omargallob@gmail.com>
2011-03-16 14:02:22 +0100

    forms with black text

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -9,7 +9,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_id = 'admin'  
     primary.item :home, t('nav.admin.overview'), admin_root_path
-    #primary.item :logout, "Logout", destroy_user_session_path
+    primary.item :logout, "Logout", destroy_admin_session_path
   end
 
 end
