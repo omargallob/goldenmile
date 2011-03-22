1a4c973801339821a954deb623ddcbd08f84a114
omar gallo <omargallob@gmail.com>
2011-03-22 20:43:33 +0100

    home dynamic left column

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+   # @section = Section.find(:first,:conditions =>{:name => "Property"})
+    @categories  = Page.all.map{|c| c.category }.uniq 
+    @pages = []
+    @categories.each_with_index do |c,i|
+      @pages[i] = Page.find(:all,:conditions =>{:category => "#{c}"})
+    end
+    #@pages = Page.find(:all,:conditions =>{:category => "Property Info"})
   end
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,6 +2,6 @@ class Section
   include Mongoid::Document
   field :name
   #embeds_many :pages
-  references_many :pages,:dependent => :destroy
+  references_many :pages,:dependent => :destroy, :inverse_of => :section
   index "pages.category"
 end
